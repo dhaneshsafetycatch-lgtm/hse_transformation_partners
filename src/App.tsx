375f8f3a51004adf17ef2ce3ffb700590e48714a
dhaneshsafetycatch-lgtm <dhanesh.safetycatch@gmail.com>
2026-06-22 16:29:31 +0530

    Start repository

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import WhatsAppFloat from './components/WhatsAppFloat';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ServicesPage from './pages/ServicesPage';
+import TrainingPage from './pages/TrainingPage';
+import DigitalPage from './pages/DigitalPage';
+import ContactPage from './pages/ContactPage';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo({ top: 0, behavior: 'instant' as ScrollBehavior }); }, [pathname]);
+  return null;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <div className="min-h-screen flex flex-col bg-white">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/training" element={<TrainingPage />} />
+            <Route path="/digital" element={<DigitalPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </main>
+        <Footer />
+        <WhatsAppFloat />
+      </div>
+    </BrowserRouter>
+  );
+}
